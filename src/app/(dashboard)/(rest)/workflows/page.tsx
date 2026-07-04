926668306c30207161944cd4fa4c2e63efb54e28
Sambit13 <sambitranjan0123@gmail.com>
2026-07-04 22:29:04 +0530

    13- workflow-UI

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -3,7 +3,7 @@ import { requireAuth } from '@/lib/auth-utils'
 import { HydrateClient } from '@/trpc/server';
 import { ErrorBoundary } from "react-error-boundary";
 import React, { Suspense } from 'react'
-import { WorkflowsContainer, WorkflowsList } from '@/features/workflows/components/workflows';
+import { WorkflowsContainer, WorkflowsError, WorkflowsList, WorkflowsLoading } from '@/features/workflows/components/workflows';
 import { SearchParams } from 'nuqs/server';
 import { workflowsParamsLoader } from '@/features/workflows/server/params-loader';
 
@@ -20,8 +20,8 @@ const page = async({ searchParams } : Props) => {
   return (
     <WorkflowsContainer>
    <HydrateClient>
-      <ErrorBoundary fallback={<p>Error!</p>}>
-        <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<WorkflowsError/>}>
+        <Suspense fallback={<WorkflowsLoading/>}>
           <WorkflowsList/>
         </Suspense>
       </ErrorBoundary>
