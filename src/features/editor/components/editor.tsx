8d10146751db5ae350130d3c9ec56db2f4a2a180
Sambit13 <sambitranjan0123@gmail.com>
2026-07-14 16:08:20 +0530

    18- node-execution

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
 import { 
@@ -23,6 +23,8 @@ import { nodeComponents } from '@/config/node-component';
 import { AddNodeButton } from './add-node-button';
 import { useSetAtom } from 'jotai';
 import { editorAtom } from '../store/atoms';
+import { NodeType  } from '@/generated/prisma';
+import { ExecuteWorkflowButton } from './execute-workflow-button';
 
 
 export const EditorLoading = () => {
@@ -63,6 +65,10 @@ const setEditor = useSetAtom(editorAtom)
     [],
   );
 
+  const hasManualTrigger = useMemo(() => {
+    return nodes.some((node) => node.type === NodeType.MANUAL_TRIGGER);
+  }, [nodes]);
+
   return (
     <div className='size-full'>
         <ReactFlow
@@ -75,7 +81,6 @@ const setEditor = useSetAtom(editorAtom)
             onInit={setEditor}
             fitView
             snapGrid={[10,10]}
-            snapToGrid
             panOnScroll
             panOnDrag={false}
             selectionOnDrag
@@ -86,6 +91,11 @@ const setEditor = useSetAtom(editorAtom)
             <Panel position='top-right'>
                 <AddNodeButton/>
             </Panel>
+            {hasManualTrigger && (
+                <Panel position='bottom-center'>
+                    <ExecuteWorkflowButton workflowId={workflowId} />
+                </Panel>
+            )}
             </ReactFlow>
 
     </div>
